b1c0889419ab4978a8807bf9eba41870651ab6d1
Osman Setuval <osman@ntsaobraz.com.br>
2019-11-01 17:53:36 -0300

    adicionado o desafio 04

---
+++ desafio04/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import facebook from '../assets/facebook.png';
+
+function Header(){
+  return (
+    <header>
+      <img src={facebook} />      
+    </header>
+  );
+}
+
+export default Header;
